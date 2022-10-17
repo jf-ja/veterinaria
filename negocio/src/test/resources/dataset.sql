a894c1eb46b6f0f56246d5ed0f3d7cd1a4e73d2e
lolEstraski <carlos1andres20@gmail.com>
2022-10-17 15:59:10 -0500

    se craron los dataset.sql teenr encuenta lo delos telefonos  se modifico  nuevamente  el dataset

--- negocio/src/test/resources/dataset.sql
+++ negocio/src/test/resources/dataset.sql
@@ -1,30 +1,30 @@
 insert into administrador values (21345,"camilo","camilo.@gmail.com","2123")
 insert into administrador values (122344,"dayana","dayana.@gmail.com","233")
-insert into administrador values (21345,"martinezz","martinez.@gmail.com","2134")
-insert into administrador values (21345,"luisa","luisa.@gmail.com","2123")
-insert into administrador values (21345,"steban","steban.@gmail.com","2123")
+insert into administrador values (233,"martinezz","martinez.@gmail.com","2134")
+insert into administrador values (2132145,"luisa","luisa.@gmail.com","2123")
+insert into administrador values (2137745,"steban","steban.@gmail.com","2123")
 
 insert into cliente values (1007303046,"miguel","luism.lopez@gmail.com","6365351")
-insert into cliente values (100730304,"123pepito","andres@gmail.com","63655")
+insert into cliente values (1010730304,"123pepito","andres@gmail.com","63655")
 insert into cliente values (1007303048,"veronica","veronica.@gmail.com","6661")
-insert into cliente values (1007303049,"johana","johana.@gmail.com","1663531")
-insert into cliente values (1007303050,"carlos","carlos.@gmail.com","12334")
-insert into cliente values (1007303051,"mellamoAlex","alex.@gmail.com")
+insert into cliente values (107303649,"johana","johana.@gmail.com","1663531")
+insert into cliente values (100303650,"carlos","carlos.@gmail.com","12334")
+insert into cliente values (100703451,"mellamoAlex","alex.@gmail.com")
 insert into cliente values (1007303052,"Karen123","karen.@gmail.com","1234233")
 
 
 insert into compra values(123444,"03/10/2022",321.123)
-insert into compra values("compra02","03/10/2022",131.313)
-insert into compra values("compra03","03/10/2022",166.890)
-insert into compra values("compra04","03/10/2022",165.5)
-insert into compra values("compra05","03/10/2022",18.000)
+insert into compra values(763,"03/10/2022",131.313)
+insert into compra values(8727,"03/10/2022",166.890)
+insert into compra values(,7351,"03/10/2022",165.5)
+insert into compra values(8371,"03/10/2022",18.000)
 
 
 insert into empleado values (21345,"daniel","daniel.@gmail.com","1332134")
 insert into empleado values (122344,"camila","camila.@gmail.com","12344")
-insert into empleado values (21345,"laura","laura.@gmail.com","2134444")
-insert into empleado values (21345,"veronica","veronica.@gmail.com","121133")
-insert into empelado values (21345,"carol","carol.@gmail.com","134311")
+insert into empleado values (1234,"laura","laura.@gmail.com","2134444")
+insert into empleado values (211231345,"veronica","veronica.@gmail.com","121133")
+insert into empelado values (211345,"carol","carol.@gmail.com","134311")
 
 
 
@@ -36,16 +36,14 @@ insert into consulta values (88943,"paseo","03/14/2022","se llevo el perro a car
 
 insert into mascota values (4343,"rambo ","15anios",40kg","bulldog)
 insert into mascota values (12333,"luis","12anios","32kg"," labrador")
-insert into mascota values (6654,"luna","5anios","25kg","gran danes")
-insert into mascota values (6654,"akira","3anios","25kg","pitbull")
-insert into mascota values (6654,"goleat","8anios","25kg","pastor aleman")
+insert into mascota values (812654,"luna","5anios","25kg","gran danes")
+insert into mascota values (763454,"akira","3anios","25kg","pitbull")
+insert into mascota values (662354,"goleat","8anios","25kg","pastor aleman")
 
-insert into persona values (12344,"camila","camila@gmail.com","1233")
-insert into persona values (12344,"dayana","dayana@gmail.com","22333")
-insert into persona values (12344,"lorena","lorena@gmail.com","23444444")
-insert into persona values (12344,"felipe","felipe@gmail.com","212344122")
-
-
+insert into persona values (112344,"camila","camila@gmail.com","1233")
+insert into persona values (142344,"dayana","dayana@gmail.com","22333")
+insert into persona values (122344,"lorena","lorena@gmail.com","23444444")
+insert into persona values (123344,"felipe","felipe@gmail.com","212344122")
 
 
 
@@ -53,9 +51,11 @@ insert into persona values (12344,"felipe","felipe@gmail.com","212344122")
 
 
 
-insert into producto values (5663,"pelota",16000,4,"juguete")
-insert into producto values (5663,"collar",16000,4,"atuendo")
-insert into producto values (5663,"amoxicilina",18000,3,"medicina")
+
+
+insert into producto values (536163,"pelota",16000,4,"juguete")
+insert into producto values (56263,"collar",16000,4,"atuendo")
+insert into producto values (51631163,"amoxicilina",18000,3,"medicina")
 
 
 
