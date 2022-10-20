cbfcfb91eb1c9ce1b068bffed00702f08374ca7d
Juan Perdomo <juanj.perdomov@uqvirtual.edu.co>
2022-10-20 12:39:17 -0500

    Se instancio varias pruebas de usuario

--- negocio/src/test/java/co/edu/uniquindio/veterinaria/test/HistorialTest.java
+++ negocio/src/test/java/co/edu/uniquindio/veterinaria/test/HistorialTest.java
@@ -1,13 +1,38 @@
 package co.edu.uniquindio.veterinaria.test;
 
+import co.edu.uniquindio.veterinaria.entidades.Compra;
+import co.edu.uniquindio.veterinaria.entidades.Historial;
 import co.edu.uniquindio.veterinaria.repo.HistorialRepo;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class HistorialTest {
     @Autowired
     private HistorialRepo historialRepo;
-}
+
+    @Test
+    @Sql("classpath:dataset.sql" )
+    public void registrar(){
+
+        Historial historial = new Historial();
+
+    }
+    @Test
+    @Sql("classpath:dataset.sql" )
+    public void eliminar(){
+
+    }
+    @Test
+    @Sql("classpath:dataset.sql" )
+    public void actualizar(){
+
+    }
+    @Test
+    @Sql("classpath:dataset.sql" )
+    public void obtener(){
+    }}
